7d821a53c4b8b6e0877ddb92aafa84e440201b66
Renilson Medeiros <50431134+malcoon@users.noreply.github.com>
2022-01-08 19:21:28 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+ 
+togglePassword.addEventListener('click', function (e) {
+
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+        
+  this.classList.toggle('fa-eye-slash');
+});
+
+
+// Navbar Mobile
+const hamburger = document.querySelector(".navbar-hamburger");
+const navNavbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navNavbar.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navNavbar.classList.remove("active");
+}))
+
+
+
+    // Desenvolvido por Renilson Medeiros [Malcoon] - Dezembro 2021
+
+    // Twitter: @malcoon_
+    // Instagram: @_malcoon
+    // Github: github.com/malcoon
